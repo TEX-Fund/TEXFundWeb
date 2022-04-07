a5a044a4f66f86f4244c4e553af03e8e8b774767
texfund <tex.fund@protonmail.com>
2022-04-07 10:48:11 +0800

    deploy web : V0.01:init

---
+++ assets/welcome.html.98460ce9.js
@@ -0,0 +1,15 @@
+const e={key:"v-1abeb830",path:"/welcome.html",title:"Welcome to TEX.Fund",lang:"en-US",frontmatter:{index:1,title:"Welcome to TEX.Fund",author:"TEX.Fund",date:"2022-03-28T00:00:00.000Z",category:["Guide"],tag:["Welcome"],sticky:!0,star:!0,summary:`TEX Fund : Token exchange aggregation platform.
+You can support us at:
+
+gitcoin.co :
+dorahacks.io :
+devpost.com :
+
+`,head:[["meta",{property:"og:url",content:"https://tex.fund/welcome.html"}],["meta",{property:"og:title",content:"Welcome to TEX.Fund"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:locale",content:"en-US"}],["meta",{property:"og:locale:alternate",content:"zh-CN"}],["meta",{property:"article:author",content:"TEX.Fund"}],["meta",{property:"article:tag",content:"Welcome"}],["meta",{property:"article:published_time",content:"2022-03-28T00:00:00.000Z"}]]},excerpt:`<p>TEX Fund : Token exchange aggregation platform.</p>
+<p>You can support us at:</p>
+<ol>
+<li>gitcoin.co :</li>
+<li>dorahacks.io :</li>
+<li>devpost.com :</li>
+</ol>
+`,headers:[{level:2,title:"TEX Fund",slug:"tex-fund",children:[]},{level:2,title:"Syber Connect",slug:"syber-connect",children:[]},{level:2,title:"Why TEXFund?",slug:"why-texfund",children:[]},{level:2,title:"Tech Stack to Build TEX Fund",slug:"tech-stack-to-build-tex-fund",children:[{level:3,title:"Identity Management",slug:"identity-management",children:[]},{level:3,title:"Account System",slug:"account-system",children:[]},{level:3,title:"Decentralized Networks",slug:"decentralized-networks",children:[]}]}],git:{createdTime:1649299605e3,updatedTime:1649299605e3,contributors:[{name:"texfund",email:"tex.fund@protonmail.com",commits:1}]},readingTime:{minutes:.45,words:134},filePathRelative:"welcome.md"};export{e as data};
